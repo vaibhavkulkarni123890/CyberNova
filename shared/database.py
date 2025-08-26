28264472df1846b5d540b3de168ae9526f4c979e
vaibhavkulkarni123890 <146406885+vaibhavkulkarni123890@users.noreply.github.com>
2025-08-26 20:20:58 +0530

    Update database.py

--- shared/database.py
+++ shared/database.py
@@ -5,7 +5,7 @@ import os
 
 MYSQL_URL = os.getenv("MYSQL_URL", "mysql+pymysql://root:OgdeXuuOLQPDnMsMqxXlcNVUvFKSgPaL@cybernova.railway.internal:3306/railway")
 
-engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
+engine = create_engine(MYSQL_URL, echo=False, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
 Base = declarative_base()
 
@@ -20,5 +20,6 @@ def init_db():
     # Import models so Base.metadata knows about them
     import shared.models  # noqa
     Base.metadata.create_all(bind=engine)
+
 
 
