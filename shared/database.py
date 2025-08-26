d51e91a62a355647dcb5ecc16ca60987eb7c8b9b
vaibhavkulkarni123890 <146406885+vaibhavkulkarni123890@users.noreply.github.com>
2025-08-26 20:14:58 +0530

    Update database.py

--- shared/database.py
+++ shared/database.py
@@ -3,9 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-MYSQL_URL = os.getenv("MYSQL_URL", "mysql://root:OgdeXuuOLQPDnMsMqxXlcNVUvFKSgPaL@cybernova.railway.internal:3306/railway")
+MYSQL_URL = os.getenv("MYSQL_URL", "mysql+pymysql://root:OgdeXuuOLQPDnMsMqxXlcNVUvFKSgPaL@cybernova.railway.internal:3306/railway")
 
-engine = create_engine(MYSQL_URL, echo=False, pool_pre_ping=True, future=True)
+engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
 Base = declarative_base()
 
@@ -20,4 +20,5 @@ def init_db():
     # Import models so Base.metadata knows about them
     import shared.models  # noqa
     Base.metadata.create_all(bind=engine)
+
 
